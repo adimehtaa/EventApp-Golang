e782177b9c3ad31438c02c795b5843d810cba495
aditya mehta <adityamehta242@gmail.com>
2026-01-12 01:41:18 +0530

    add migration code and generate migrate.sql

--- backend/cmd/migrate/main.go
+++ backend/cmd/migrate/main.go
@@ -1,12 +1,70 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
+
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/sqlite3"
+	"github.com/golang-migrate/migrate/source/file"
 )
 
 func main() {
-	if len(os.Args) > 2 {
+	if len(os.Args) < 2 {
 		log.Fatal("Plese provie a migration direction 'up' or 'down' ")
 	}
+
+	direction := os.Args[1]
+
+	db, err := sql.Open("sqlite3", "./data.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	instance, err := sqlite3.WithInstance(db, &sqlite3.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fSrc, err := (&file.File{}).Open("cmd/migrate/migrations")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithInstance("file", fSrc, "sqlite3", instance)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+
+	case "down":
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+
+	default:
+		log.Fatal("Invalid migration direction. Use 'up' or 'down'")
+	}
+
 }
+
+/**
+# run this command to generate migration file
+	(Golang migrate need to be install starting migration)
+	- migrate create -ext sql -dir ./cmd/migrate/migrations -seq table_name
+	for example
+	: migrate create -ext sql -dir ./cmd/migrate/migrations -seq create_users_table
+
+*/
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,10 @@
 module app-event
 
 go 1.24.5
+
+require github.com/golang-migrate/migrate v3.5.4+incompatible
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/mattn/go-sqlite3 v1.14.33 // indirect
+)
