569c802e92c99796e2a4bf0b345ff68bc138a58a
aditya mehta <adityamehta242@gmail.com>
2026-01-13 00:01:54 +0530

    setup server and read env value

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"app-event/internal/database"
+	"app-event/internal/env"
 	"database/sql"
 	"log"
 )
+
+type application struct {
+	port      int
+	jwtSecret string
+	model     database.Models
+}
 
 func main() {
 
@@ -14,4 +22,16 @@ func main() {
 	}
 
 	defer db.Close()
+
+	models := database.NewModels(db)
+
+	app := &application{
+		port:      env.GetEnvInteger("PORT", 8080),
+		jwtSecret: env.GetEnvString("JWT_SECRET", "Some-Secret-808080"),
+		model:     models,
+	}
+
+	if err := app.serve(); err != nil {
+		log.Fatal(err)
+	}
 }
